8666ec6e69b15aae9b1d8fa1eedefca9bda40cc4
Yauheni-Varenik <yauheni.varenik@gmail.com>
2023-03-03 18:22:15 +0300

    first-main-branch

---
+++ lesson4_1.py
@@ -0,0 +1,7 @@
+
+
+for line in range(0, 10):
+    for number in range(0, 10):
+        print(line, "*", number, "=", line * number, end = "\t")
+    print("")
+name = int(input("Введите что либо для продолжения"))
