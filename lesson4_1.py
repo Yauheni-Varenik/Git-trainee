d60c233a833039af4dd2e6d9641670d830faa67c
Yauheni-Varenik <yauheni.varenik@gmail.com>
2023-03-07 19:17:06 +0300

    lacal-py

--- lesson4_1.py
+++ lesson4_1.py
@@ -4,4 +4,4 @@ for line in range(0, 10):
     for number in range(0, 10):
         print(line, "*", number, "=", line * number, end = "\t")
     print("")
-name = int(input("Введите что либо для продолжения"))
+name = int(input("Введите что либо для "))
