f3069f2a0d09b1ce18abeb4601719aa3ed758d68
Yauheni-Varenik <yauheni.varenik@gmail.com>
2023-03-10 18:01:14 +0300

    final-vers3

--- lesson4_1.py
+++ lesson4_1.py
@@ -3,6 +3,6 @@
 for line in range(0, 10):
     for number in range(0, 10):
         print(line, "*", number, "=", line * number, end = "\t")
-    print("")
-name = int(input("Введите что либо для "))
+    print("fghfghfgh")
+name = int(input("Введите что либо дляfghgfhgf "))
 
